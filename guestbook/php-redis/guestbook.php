b49d480ff37952913c8551a3e9cdc48e6c1f842a
cramte <42623182+cramte@users.noreply.github.com>
2020-05-19 22:41:21 +0200

    Add cpu load code

--- guestbook/php-redis/guestbook.php
+++ guestbook/php-redis/guestbook.php
@@ -22,6 +22,27 @@ if (isset($_GET['cmd']) === true) {
 
     $client->set($_GET['key'], $_GET['value']);
     print('{"message": "Updated"}');
+  } elseif ($_GET['cmd'] == 'stress') {
+    set_time_limit(300);
+    $runtime_limit = 1000 * 60;
+    if (isset($_GET['len']) === true) {
+        $runtime_limit = 1000 * ((int) $_GET['len']);
+    }
+    $limit = 10;
+    if (isset($_GET['cpu']) === true) {
+        $limit = ((int) $_GET['cpu']);
+    }
+    $begin = microtime(true)*1000;
+    $current_time = $begin;
+    $sleep =  1000 * (100 - $limit);
+    while ($current_time - $begin < $runtime_limit) {
+      usleep($sleep);
+      $loop_start = microtime(true)*1000;
+      while ($current_time - $loop_start < $limit) {
+        $current_time = microtime(true) * 1000;
+      }
+    }
+    print('{}');
   } else {
     $host = 'redis-slave';
     if (getenv('GET_HOSTS_FROM') == 'env') {
